120495aaad4789af3e3717a8b6fc41dd13d865c5
Yamasaki Sakiko <sechia.saki@gmail.com>
2025-08-01 00:06:47 +0900

    最終調整

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,11 @@
 import { defineConfig } from 'astro/config';
 
+const isProd = process.env.NODE_ENV === 'production';
+
 export default defineConfig({
   site: 'https://ring013.github.io/my-blog',
-  base: '/my-blog/',
-  output: 'static',
+  base: isProd ? '/my-blog/' : '/',
+  build: {
+    // 必要に応じて設定を変更
+  },
 });

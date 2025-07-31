4951bfe11d79dd05dd6b01581d133ae16bd13ae7
Yamasaki Sakiko <sechia.saki@gmail.com>
2025-08-01 00:35:09 +0900

    初期コミット：ブログサイト構築

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,7 @@
 import { defineConfig } from 'astro/config';
 
-const isProd = process.env.NODE_ENV === 'production';
-
 export default defineConfig({
-  site: 'https://ring013.github.io/my-blog',
-  base: isProd ? '/my-blog/' : '/',
-  build: {
-    // 必要に応じて設定を変更
-  },
+  site: 'https://ring013.github.io/',
+  base: '/my-blog/',
 });
+
